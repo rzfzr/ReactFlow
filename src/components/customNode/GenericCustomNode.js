23e4aa7effb45a513214e0b7298dbbf96eeba80e
rzfzr <capitao.rafa@gmail.com>
2024-04-03 10:37:20 -0300

    Displaying name on custom node

--- src/components/customNode/GenericCustomNode.js
+++ src/components/customNode/GenericCustomNode.js
@@ -3,15 +3,6 @@ import { Handle, HandleType, Position } from 'reactflow';
 import './GenericCustomNode.css';
 
 export default function GenericCustomNode(props) {
-  const onChange = useCallback((evt) => {
-    console.log(evt.target.value);
-  }, []);
-
-  const onDragStart = (event, node) => {
-    event.dataTransfer.setData('application/reactflow', JSON.stringify(node));
-    event.dataTransfer.effectAllowed = 'move';
-  };
-
   const handleStyle = {
     // position: 'relative',
     // width: '10px',
@@ -20,16 +11,22 @@ export default function GenericCustomNode(props) {
     // borderRadius: '0',
     // zIndex: '10',
   };
-  console.log(props.data.name);
   return (
     <div className="component-body"
       draggable={props.isDraggable}
     >
       <div className="component-title">
-        <p>{props.data.name}</p>
+        <p>{props.data.label}</p>
       </div>
       <div className="component-content">
         <div className="component-port">
+          <Handle
+            type={props.data.types}
+            position={Position.Left}
+            id={props.id}
+            isConnectable={true}
+            style={handleStyle}
+          />
           <Handle
             type={props.data.types}
             position={Position.Left}
@@ -48,7 +45,6 @@ export default function GenericCustomNode(props) {
           />
         </div>
       </div>
-      <input id="text" name="text" onChange={onChange} className="nodrag" /> 
     </div>
   );
 }
